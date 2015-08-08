a2b11428d685ab9d084109a99ada24400136f26e
Rob <snintendog@gmail.com>
2015-08-08 13:36:39 -0400

    Update and rename YPARWL to YPARWL.user.js
    Added the mouse hover fix for testing purposes(Would still like to get a better fix one that would work on embedded videos as well)
    minor alterations in response to youtube's minor UI fixes. 
    Disabled the YTCenter fix for video content(Breaks full sceen with/without YTC anyways)
    Changed to a .user.js file for easier testing. (Have been using it as a personal fix so its been optimized for 1366x768 Firefox 39.0.3 Hope fully is still Universal)

--- Dev/YPARWL.user.js
+++ Dev/YPARWL.user.js
@@ -7,6 +7,13 @@
 // @version     0.0.1
 // @grant       none
 // ==/UserScript==
+
+(function() {
+	//mouse over the controls to update them (this seems to also work with 2000ms instead of 1000ms)
+	var evObj = document.createEvent('Events');
+	evObj.initEvent("mousemove", true, false);
+	var moviePlayer = document.getElementById("movie_player");
+    setInterval(function() { moviePlayer.dispatchEvent(evObj); }, 1000);
 	
 var origbutt = document.getElementsByClassName('ytp-watch-later-button ytp-button')[0]
 var watchlaterbutt = origbutt.cloneNode(1);
@@ -30,18 +37,18 @@ document.getElementsByClassName('ytp-chrome-controls')[0].appendChild(watchlater
 		".ytp-gradient-bottom {height: 0px !important;}",
 		".ytp-chrome-controls{ height: 30px !important; line-height: 30px !important;Display: block !important; }",
 		".ytp-progress-bar-container { background: black !important;display: block !important;bottom: 27px !important; }",
-		".watch-stage-mode #movie_player { height: calc(100% + 30px)!important; }",
+		".watch-stage-mode #movie_player { height: calc(100% + 31px)!important; }",
 		":not(.watch-stage-mode) #movie_player { height: calc(100% + 30px)!important; }",
 		"#watch7-content {transform: translateY(30px); }",
 		"body:not(.ytwp-window-player) .watch-stage-mode #watch7-sidebar-contents{ transform: translateY(48px); }",
 		"body:not(.ytwp-window-player) .watch-stage-mode #watch7-content { transform: translateY(48px); }",
 		".ytp-big-mode video { height: calc(100% + 33px)!important; }",
 		".watch-stage-mode #theater-background{  bottom: -34px!important; left: 0px!important; position: absolute!important; background-color: black!important; height: 0px!important; width: 100%!important; }",
-		".html5-video-container { height: 95%!important; }",
+		".html5-video-container { height: 91%!important; }",
 		".ytp-button.ytp-cards-button { top: 0% !important;}",
-		".watch-wide .watch-playlist{ transform: translateY(83%)!important; margin-bottom: 30px; }", //Fixes wide+playlist/mix
-		".html5-video-content{top: 0px !important; }", //-3% with YTCenter dev >V 531 on
-		".video-stream{ margin-top: -3% !important; }", //-3% with YTCenter dev >V 531 on
+		".watch-wide .watch-playlist{ transform: translateY(89%)!important; margin-bottom: 30px; }", //Fixes wide+playlist/mix
+		".html5-video-content{top: 0% !important; }", //-3% with YTCenter dev >V 531 on now fixed
+	//	".video-stream{ margin-top: -3% !important; }", //-3% with YTCenter dev >V 531 on 0% without unknown cause bug does not affect fullscreen but fix does
 	].join("\n");
     document.head.appendChild(css);
 })();

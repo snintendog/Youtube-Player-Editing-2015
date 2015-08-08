decaa3def40ae30abf91bf584fd5a3c26289a81d
Rob <snintendog@gmail.com>
2015-08-08 18:17:44 -0400

    Update YPARWL.user.js
    Due to the minor changes -2% works better for the YTC compatibility fix
    added HD button fix while in Full screen
    Organized Css into categories

--- Dev/YPARWL.user.js
+++ Dev/YPARWL.user.js
@@ -14,7 +14,8 @@
 	evObj.initEvent("mousemove", true, false);
 	var moviePlayer = document.getElementById("movie_player");
     setInterval(function() { moviePlayer.dispatchEvent(evObj); }, 1000);
-	
+    
+//Watch Later Restore	
 var origbutt = document.getElementsByClassName('ytp-watch-later-button ytp-button')[0]
 var watchlaterbutt = origbutt.cloneNode(1);
 
@@ -31,24 +32,36 @@ document.getElementsByClassName('ytp-chrome-controls')[0].appendChild(watchlater
 	var css = document.createElement('style');
     css.type = "text/css";
     css.textContent = [
+		///*Auto Hide off*/
 		"div.ytp-chrome-bottom { opacity: 1 !important; display: block !important; width: 100% !important; left: 0% !important; height: 30px !important; background: black !important; bottom: -2px !important; }",
+		
+		///*The Player Fixes*/
 		".ytp-subtitles-player-content { bottom: 40px !important; }", //subtitles fix no longer bounces or hides on mouse hover
-		".ytp-watch-later-button.ytp-button{ padding: 2px !important}", //cleans up the watch later restore button to match the rest of the buttons sizes
-		".ytp-gradient-bottom {height: 0px !important;}",
-		".ytp-chrome-controls{ height: 30px !important; line-height: 30px !important;Display: block !important; }",
-		".ytp-progress-bar-container { background: black !important;display: block !important;bottom: 27px !important; }",
-		".watch-stage-mode #movie_player { height: calc(100% + 31px)!important; }",
+		".ytp-button.ytp-cards-button { top: 0% !important;}",		
 		":not(.watch-stage-mode) #movie_player { height: calc(100% + 30px)!important; }",
 		"#watch7-content {transform: translateY(30px); }",
+
+		///*Control Style*/
+		".ytp-chrome-controls{ height: 30px !important; line-height: 30px !important;Display: block !important; }",
+		".ytp-progress-bar-container { background: black !important;display: block !important;bottom: 27px !important; }",
+		".ytp-gradient-bottom {height: 0px !important;}",
+		".ytp-watch-later-button.ytp-button{ padding: 2px !important}", //cleans up the watch later restore button to match the rest of the buttons sizes
+
+		///*Theater Mode Fix*/
+		".watch-wide .watch-playlist{ transform: translateY(89%)!important; margin-bottom: 30px; }", //Fixes wide+playlist/mix
+		".watch-stage-mode #theater-background{  bottom: -34px!important; left: 0px!important; position: absolute!important; background-color: black!important; height: 0px!important; width: 100%!important; }",
 		"body:not(.ytwp-window-player) .watch-stage-mode #watch7-sidebar-contents{ transform: translateY(48px); }",
 		"body:not(.ytwp-window-player) .watch-stage-mode #watch7-content { transform: translateY(48px); }",
+		".watch-stage-mode #movie_player { height: calc(100% + 31px)!important; }",
+	
+		///*Fullscreen Fix*/
 		".ytp-big-mode video { height: calc(100% + 33px)!important; }",
-		".watch-stage-mode #theater-background{  bottom: -34px!important; left: 0px!important; position: absolute!important; background-color: black!important; height: 0px!important; width: 100%!important; }",
 		".html5-video-container { height: 91%!important; }",
-		".ytp-button.ytp-cards-button { top: 0% !important;}",
-		".watch-wide .watch-playlist{ transform: translateY(89%)!important; margin-bottom: 30px; }", //Fixes wide+playlist/mix
-		".html5-video-content{top: 0% !important; }", //-3% with YTCenter dev >V 531 on now fixed
-	//	".video-stream{ margin-top: -3% !important; }", //-3% with YTCenter dev >V 531 on 0% without unknown cause bug does not affect fullscreen but fix does
+    		".ytp-big-mode .ytp-settings-button.ytp-hd-quality-badge::after,.ytp-big-mode .ytp-settings-button.ytp-4k-quality-badge::after,.ytp-big-mode .ytp-settings-button.ytp-5k-quality-badge::after,.ytp-big-mode .ytp-settings-button.ytp-8k-quality-badge::after{ content:'HD'!important; height: 20% ; width: 28% ; font-size: 50%; line-height: 50%; }",
+
+		///*YTC Compatibility Fixes*/
+		".html5-video-content{top: 0% !important; }", //-2% with YTCenter dev >V 531 on now fixed
+	//	".video-stream{ margin-top: -2% !important; }", //-2% with YTCenter dev >V 531 on 0% without unknown cause bug does not affect fullscreen but fix does
 	].join("\n");
     document.head.appendChild(css);
 })();
